3d2bdfaf78a1bb7a97d237719c7de1109956f93d
Riku Heikkinen <riku.heikkinen94@gmail.com>
2020-03-27 23:09:02 +0200

    started making the table reservation feature. Still missing the date picker part

--- client/app/src/main/java/hungry/travelersapp/hungry_travellers_app/ui/reserve/ReserveFragment.java
+++ client/app/src/main/java/hungry/travelersapp/hungry_travellers_app/ui/reserve/ReserveFragment.java
@@ -1,9 +1,16 @@
 package hungry.travelersapp.hungry_travellers_app.ui.reserve;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,19 +19,35 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
 import hungry.travelersapp.hungry_travellers_app.R;
 
 public class ReserveFragment extends Fragment {
 
     private ReserveViewModel reserveViewModel;
 
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         reserveViewModel =
                 ViewModelProviders.of(this).get(ReserveViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_reserve, container, false);
+        final View root = inflater.inflate(R.layout.fragment_reserve, container, false);
         final TextView textView = root.findViewById(R.id.text_reserve);
-        final TextView textView2 = root.findViewById(R.id.text_testi);
+        final TextView textView2 = root.findViewById(R.id.text_people);
+        final TextView textView3 = root.findViewById(R.id.text_name);
+        final TextView textView4 = root.findViewById(R.id.text_number);
+        //final TextView textView2 = root.findViewById(R.id.text_testi);
+        final EditText userInput = root.findViewById(R.id.reserve_people);
+        final EditText userInput2 = root.findViewById(R.id.reserve_name);
+        final EditText userInput3 = root.findViewById(R.id.reserve_phoneNumber);
+        final Button submitBtn = root.findViewById(R.id.ButtonSendFeedback);
+
+
 
         reserveViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -38,6 +61,43 @@ public class ReserveFragment extends Fragment {
                 textView2.setText(s);
             }
         });
+        reserveViewModel.getText3().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView3.setText(s);
+            }
+        });
+        reserveViewModel.getText4().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView4.setText(s);
+            }
+        });
+
+       /* reserveViewModel.getText2().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView2.setText(s);
+            }
+        }); */
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String peopleAmount = userInput.getText().toString();
+                String reservationName = userInput2.getText().toString();
+                String reservationPhoneNumber = userInput3.getText().toString();
+                Log.d(peopleAmount, "dasdsa");
+                Log.d(reservationName, "kappa kappa");
+                Log.d(reservationPhoneNumber, "numero");
+            }
+        });
+
+       // final EditText userInput = root.findViewById(R.id.reserve_people);
+
+
+
         return root;
     }
+
 }
--- client/app/src/main/java/hungry/travelersapp/hungry_travellers_app/ui/reserve/ReserveViewModel.java
+++ client/app/src/main/java/hungry/travelersapp/hungry_travellers_app/ui/reserve/ReserveViewModel.java
@@ -1,28 +1,48 @@
 package hungry.travelersapp.hungry_travellers_app.ui.reserve;
 
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.Calendar;
+
 public class ReserveViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+   // private MutableLiveData<String> mText2;
     private MutableLiveData<String> mText2;
+    private MutableLiveData<String> mText3;
+    private MutableLiveData<String> mText4;
 
 
     public ReserveViewModel() {
         mText = new MutableLiveData<>();
+       // mText2 = new MutableLiveData<>();
         mText2 = new MutableLiveData<>();
+        mText3 = new MutableLiveData<>();
+        mText4 = new MutableLiveData<>();
 
-        mText.setValue("This is reservation fragment");
-        mText2.setValue("This is the test");
+        mText.setValue("Reserve a table");
+       // mText2.setValue("This is the test");
+        mText2.setValue("People");
+        mText3.setValue("Your name");
+        mText4.setValue("Your phone number");
+
     }
+
 
     public LiveData<String> getText() {
         return mText;
     }
-    public LiveData<String> getText2() {
+   /* public LiveData<String> getText2() {
         return mText2;
-    }
+    }*/
+   public LiveData<String> getText2() { return mText2; }
+    public LiveData<String> getText3() { return mText3; }
+    public LiveData<String> getText4() { return mText4; }
 
 }
