9c1948976aba9a45d2989e5457a4adda4397a333
Tiina Matala <t7mati01@students.oamk.fi>
2020-04-18 18:27:07 +0300

    almost finished with sign up

--- client/app/src/main/java/hungry/travelersapp/hungry_travellers_app/ui/profile/SignUpActivity.java
+++ client/app/src/main/java/hungry/travelersapp/hungry_travellers_app/ui/profile/SignUpActivity.java
@@ -1,5 +1,6 @@
 package hungry.travelersapp.hungry_travellers_app.ui.profile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,23 +8,115 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 import hungry.travelersapp.hungry_travellers_app.R;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextName, editTextEmail, editTextPhone, editTextAddress, editTextPassword ;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        final EditText signUpInput = findViewById(R.id.write_nameOfUser);
-        final EditText signUpInput2 = findViewById(R.id.write_email);
-        final EditText signUpInput3 = findViewById(R.id.write_phone);
-        final EditText signUpInput4 = findViewById(R.id.write_address);
-        final EditText signUpInput5 = findViewById(R.id.login_password);
-        final Button signUpBtn = findViewById(R.id.ButtonSignUp);
 
-        signUpBtn.setOnClickListener(new View.OnClickListener() {
+        editTextName = findViewById(R.id.write_nameOfUser);
+        editTextEmail = findViewById(R.id.write_email);
+        editTextPhone = findViewById(R.id.write_phone);
+        editTextAddress = findViewById(R.id.write_address);
+        editTextPassword = findViewById(R.id.login_password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.ButtonSignUp).setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() != null) {
+            //handle the logged in user
+        }
+    }
+
+    private void registerUser() {
+        final String name = editTextName.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+        final String phone = editTextPhone.getText().toString().trim();
+        final String address = editTextAddress.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (name.isEmpty()) {
+            Toast.makeText(getApplicationContext(),"Please fill in your full name", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please fill in your email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (phone.length() != 10) {
+            Toast.makeText(getApplicationContext(), "Please enter valid phone number, number should be 10 characters", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (address.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please fill in address for home delivery", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Please fill in password minimum of 6 characters", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            User user = new User(
+                                    name,
+                                    email,
+                                    phone,
+                                    address
+                            );
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Registration successful", Toast.LENGTH_LONG).show();
+                                    }
+                                    else {
+                                        Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+     /*   signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String usersName = signUpInput.getText().toString();
@@ -38,6 +131,16 @@ public class SignUpActivity extends AppCompatActivity {
                 Log.d(usersAddress, "test address");
                 Log.d(usersPassword, "password");
             }
-        });
+        }); */
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ButtonSignUp:
+                registerUser();
+                break;
+        }
+
     }
 }
---
+++ client/app/src/main/java/hungry/travelersapp/hungry_travellers_app/ui/profile/User.java
@@ -0,0 +1,16 @@
+package hungry.travelersapp.hungry_travellers_app.ui.profile;
+
+public class User {
+    public String name, email, phone, address;
+
+    public User() {
+
+    }
+
+    public User(String name, String email, String phone, String address) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+}
