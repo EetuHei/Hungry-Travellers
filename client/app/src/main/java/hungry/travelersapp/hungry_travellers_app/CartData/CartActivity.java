efee0d94eb872a7bef269139ad8153f88ba183c2
Riku Heikkinen <riku.heikkinen94@gmail.com>
2020-04-22 21:20:28 +0300

    added button to go to home delivery or pick up food from restaurant to shopping cart

--- client/app/src/main/java/hungry/travelersapp/hungry_travellers_app/CartData/CartActivity.java
+++ client/app/src/main/java/hungry/travelersapp/hungry_travellers_app/CartData/CartActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,28 +21,61 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import hungry.travelersapp.hungry_travellers_app.MainActivity;
 import hungry.travelersapp.hungry_travellers_app.R;
+import hungry.travelersapp.hungry_travellers_app.ui.delivery.DeliveryActivity;
+import hungry.travelersapp.hungry_travellers_app.ui.delivery.Orders;
 import hungry.travelersapp.hungry_travellers_app.ui.menu.Food;
 
 public class CartActivity extends AppCompatActivity {
 
+    public String orderName = "";
+    public String orderAddress = "";
+    public String orderPhoneNumber = "";
     RecyclerView MenuRecycler;
     List<CartModel> MenuList;
     CartModel cartModel;
     CartAdapter cartAdapter;
     DatabaseReference CartRef;
+    DatabaseReference databaseOrders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
 
-        MenuList=new ArrayList<CartModel>();
-        MenuRecycler=findViewById(R.id.recyclerview);
+        databaseOrders = FirebaseDatabase.getInstance().getReference("Orders");
+
+        MenuList = new ArrayList<CartModel>();
+        MenuRecycler = findViewById(R.id.recyclerview);
         MenuRecycler.setHasFixedSize(true);
         MenuRecycler.setLayoutManager(new LinearLayoutManager(this));
 
-        CartRef= FirebaseDatabase.getInstance().getReference("Cart");
+        Button homeDelivery = findViewById(R.id.homeDelivery);
+        Button pickUp = findViewById(R.id.pickUp);
+
+        homeDelivery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDelivery();
+            }
+        });
+
+        pickUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String id = databaseOrders.push().getKey();
+
+                Orders order = new Orders(id, orderName, orderAddress, orderPhoneNumber);
+
+                databaseOrders.child(id).setValue(order);
+
+                openPickUp();
+            }
+        });
+
+        CartRef = FirebaseDatabase.getInstance().getReference("Cart");
         CartRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -48,7 +86,7 @@ public class CartActivity extends AppCompatActivity {
                     for (DataSnapshot npsnapshot : dataSnapshot.getChildren()) {
                         cartModel = npsnapshot.getValue(Food.class);
                         MenuList.add(cartModel);
-                        cartAdapter=new CartAdapter(CartActivity.this,MenuList);
+                        cartAdapter = new CartAdapter(CartActivity.this, MenuList);
                         MenuRecycler.setAdapter(cartAdapter);
                     }
                 }
@@ -60,6 +98,17 @@ public class CartActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    public void openDelivery() {
+        Intent intent = new Intent(CartActivity.this, DeliveryActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPickUp() {
+
+        Toast.makeText(getApplicationContext(), "Order successful!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(CartActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
